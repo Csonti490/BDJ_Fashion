bf6a570a62872d62ac88e87dae656dba15546df1
CsontiXD <113976041+Csonti490@users.noreply.github.com>
2024-10-10 19:19:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function TemaValtas(){
+    var asd = document.getElementById("tema");
+    document.documentElement.style.colorScheme = document.documentElement.style.colorScheme === 'dark' ? 'light' : 'dark';
+}
